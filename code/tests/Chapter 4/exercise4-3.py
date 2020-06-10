c69f77792da1cf7c14eb5c9a8ad918689b66c450
ll7 <sailingmaster92@gmx.de>
2020-06-10 20:08:47 +0200

    ex 4-2

--- code/tests/Chapter 4/exercise4-3.py
+++ code/tests/Chapter 4/exercise4-3.py
@@ -1,22 +1,29 @@
 import turtle
+import math
 
-def draw_triangle(t, edge):
-    for i in range(2):
-        t.fd(50)
-        t.lt(edge)
-    t.fd(50)
+def draw_triangle(t, edge, length=50):
+    
+    t.fd(length)
+    t.lt(180-edge)
+    # cos = AK / HY -> AK = cos * HY
+    AK = math.cos(edge*math.pi/180) * length
+    t.fd(2*AK)
+    t.lt(180-edge)
+    t.fd(length)
 
-def draw_pie(t, n_triangle):
+def draw_pie(t, n_triangle, length):
     ang_tri_center = 360/n_triangle
     ang_tri_out = (180-ang_tri_center)/2
     
-    draw_triangle(t, ang_tri_out)
+    for i in range (n_triangle):
+        draw_triangle(t, ang_tri_out, length)
+        t.lt(180)
     
 
 
 def main():
     bob = turtle.Turtle()
-    draw_pie(bob, 5)
+    draw_pie(bob, 3, 100)
     turtle.mainloop()
 
 if __name__ == '__main__':
